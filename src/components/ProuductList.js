2d5dde7fd54c02b8fc4d2115c323fa48f41ccf0a
nadhiya12345678 <nathiya16saraswathi@gmail.com>
2024-01-09 14:54:18 +0530

    #Working Cards

--- src/components/ProuductList.js
+++ src/components/ProuductList.js
@@ -1,116 +1,238 @@
-import './productList.css';
-import image1 from "../img/pic1.jpg";
-import image2 from "../img/pic2.jpg";
-import image3 from "../img/pic3.jpg";
-import image4 from "../img/pic4.jpg";
+// import './productList.css';
+// import image1 from "../img/pic1.jpg";
+// import image2 from "../img/pic2.jpg";
+// import image3 from "../img/pic3.jpg";
+// import image4 from "../img/pic4.jpg";
+// const ProductList = () => {
+//   return(
+//     <div className="productList">
+//       <div className='card-continer'>
+//         <div className='product'>
+//           <h2>Fugiat sit voluptate elit excepteur dolor</h2>
+//           <div className='product-card-nested'>
+//             <div className='card-nested'>
+//               <img src={image1}/>
+//               <p>Home storage</p>
+//             </div>
+//             <div className='card-nested'>
+//               <img src={image2}/>
+//               <p>Lighting solutions</p>
+//             </div>
+//             <div className='card-nested'>
+//               <img src={image3}/>
+//             </div>
+//             <div className='card-nested'>
+//               <img src={image4}/>
+//             </div>
+            
+//   <div className='textHolder'>
+//    <button>Add to Cart</button>
+//   </div>
+//           </div>
+//         </div>
+//       </div>
+//    {/* <div className='card1'> */}
+//   {/* <div className='imageHolder1'>
+//     <div className='imageContainer'>
+//       <img src={image1} alt='Vases, decor and more' className='topRight' />
+//       <img src={image2} alt='Cushion covers, bedsheets & more' className='topLeft' />
+//     </div>
+//     <div className='imageContainer'>
+//       <img src={image3} alt='Vases, decor and more' className='bottomRight' />
+//       <img src={image4} alt='Vases, decor and more' className='bottomLeft' />
+//     </div>
+//   </div> */}
+
+
+
+
+
+   
+//       <div className='card'>
+//         <p className='title'>Fugiat sit voluptate elit excepteur dolor eiusmod adipisicing ipsum labore qui sit pariatur laborum.</p>
+//         <p className='price'>200.00</p>
+//         <p className='starRating'>stars</p>
+//         <div className='imageHolder'>
+//           <img src='https://m.media-amazon.com/images/I/91bYsX41DVL._AC_SY400_.jpg' alt=''/>
+//           <br/>
+//           <br/>
+//           <button>Add to Cart</button>
+//         </div>
+//       </div>
+//       <div className='card'>
+//         <p className='title'>Fugiat sit voluptate elit excepteur dolor eiusmod adipisicing ipsum labore qui sit pariatur laborum.</p>
+//         <p className='price'>300.00</p>
+//         <p className='starRating'>stars</p>
+//         <div className='imageHolder'>
+//           <img src='https://m.media-amazon.com/images/I/81bsw6fnUiL._AC_SY400_.jpg' alt=''/>
+//           <br/>
+//           <br/>
+//           <button>Add to Cart</button>
+//         </div>
+//       </div>
+//       <div className='card'>
+//         <p className='title'>Fugiat sit voluptate elit excepteur dolor eiusmod adipisicing ipsum labore qui sit pariatur laborum.</p>
+//         <p className='price'>410.00</p>
+//         <p className='starRating'>stars</p>
+//         <div className='imageHolder'>
+//           <img src='https://m.media-amazon.com/images/I/81l3rZK4lnL._AC_SY400_.jpg' alt=''/>
+//           <br/>
+//           <br/>
+//           <button>Add to Cart</button>
+//         </div>
+//       </div>
+//       <div className='card'>
+//         <p className='title'>Fugiat sit voluptate elit excepteur dolor eiusmod adipisicing ipsum labore qui sit pariatur laborum.</p>
+//         <p className='price'>610.00</p>
+//         <p className='starRating'>stars</p>
+//         <div className='imageHolder'>
+//           <img src='https://m.media-amazon.com/images/I/81jv44QdNwL._AC_SY400_.jpg' alt=''/>
+//           <br/>
+//           <br/>
+//           <button>Add to Cart</button>
+//         </div>
+//       </div>
+//       <div className='card'>
+//         <p className='title'>Fugiat sit voluptate elit excepteur dolor eiusmod adipisicing ipsum labore qui sit pariatur laborum.</p>
+//         <p className='price'>110.00</p>
+//         <p className='starRating'>stars</p>
+//         <div className='imageHolder'>
+//           <img src='https://m.media-amazon.com/images/I/818e+fq7+BL._AC_SY400_.jpg' alt=''/>
+//           <br/>
+//           <br/>
+//           <button>Add to Cart</button>
+//         </div>
+//       </div>
+//       <div className='card'>
+//         <p className='title'>Fugiat sit voluptate elit excepteur dolor eiusmod adipisicing ipsum labore qui sit pariatur laborum.</p>
+//         <p className='price'>910.00</p>
+//         <p className='starRating'>stars</p>
+//         <div className='imageHolder'>
+//           <img src='https://m.media-amazon.com/images/I/71sBtM3Yi5L._AC_SY400_.jpg' alt=''/>
+//           <br/>
+//           <br/>
+//           <button>Add to Cart</button>
+//         </div>
+//       </div>
+//       <div className='card'>
+//         <p className='title'>Fugiat sit voluptate elit excepteur dolor eiusmod adipisicing ipsum labore qui sit pariatur laborum.</p>
+//         <p className='price'>410.00</p>
+//         <p className='starRating'>stars</p>
+//         <div className='imageHolder'>
+//           <img src='https://m.media-amazon.com/images/I/710jnzKlDTL._AC_SY400_.jpg' alt=''/>
+//           <br/>
+//           <br/>
+//           <button>Add to Cart</button>
+//         </div>
+//       </div>
+//       <div className='card'>
+//         <p className='title'>Fugiat sit voluptate elit excepteur dolor eiusmod adipisicing ipsum labore qui sit pariatur laborum.</p>
+//         <p className='price'>10.00</p>
+//         <p className='starRating'>stars</p>
+//         <div className='imageHolder'>
+//           <img src='https://m.media-amazon.com/images/I/81N7FmJhbhL._AC_SY400_.jpg' alt=''/>
+//           <br/>
+//           <br/>
+//           <button>Add to Cart</button>
+//         </div>
+//       </div>
+// </div>
+//   )
+// }
+
+// export default ProductList;
+// import React from 'react';
+// import './productList.css'; // Import your updated CSS file here
+// import image1 from '../img/pic1.jpg';
+// import image2 from '../img/pic2.jpg';
+// import image3 from '../img/pic3.jpg';
+// import image4 from '../img/pic4.jpg';
+
+// const ProductList = () => {
+//   return (
+//     <div className="productList">
+//       <div className='card-continer'>
+//         <div className='product'>
+//           <h2>Fugiat sit voluptate elit excepteur dolor</h2>
+//           <div className='product-card-nested'>
+//           <div className='card'>
+//   <img src={image1} alt='Home storage' className='product-image' />
+//   <p>Home storage</p>
+//   <div className='textHolder'>
+//     <button className='add-to-cart-btn'>Add to Cart</button>
+//   </div>
+// </div>
+
+//             <div className='card-nested'>
+//               <img src={image2} alt='Lighting solutions' className='product-image' />
+//               <p>Lighting solutions</p>
+//             </div>
+//             <div className='card-nested'>
+//               <img src={image3} alt='Vases, decor and more' className='product-image' />
+//             </div>
+//             <div className='card-nested'>
+//               <img src={image4} alt='Vases, decor and more' className='product-image' />
+//             </div>
+//             <div className='textHolder'>
+//               <button className='add-to-cart-btn'>Add to Cart</button>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ProductList;
+
+import React from 'react';
+import './productList.css'; // Import your updated CSS file here
+import image1 from '../img/pic1.jpg';
+import image2 from '../img/pic2.jpg';
+import image3 from '../img/pic3.jpg';
+import image4 from '../img/pic4.jpg';
+
 const ProductList = () => {
-  return(
+  return (
     <div className="productList">
-<div className='card'>
-  <div className='imageHolder'>
-  <img src={image1}  alt='Vases, decor and more' />
-    <img src={image2} alt='Cushion covers, bedsheets & more' />
-    <img src={image3}  alt='Vases, decor and more' />
-    <img src={image4}  alt='Vases, decor and more' />
-  </div>
-  <div className='textHolder'>
-    <p className='title'>Vases, decor and more</p>
-    <p className='subtitle'>Cushion covers, bedsheets & more</p>
-    <button>Add to Cart</button>
-  </div>
-
-
-      </div>
-      <div className='card'>
-        <p className='title'>Fugiat sit voluptate elit excepteur dolor eiusmod adipisicing ipsum labore qui sit pariatur laborum.</p>
-        <p className='price'>200.00</p>
-        <p className='starRating'>stars</p>
-        <div className='imageHolder'>
-          <img src='https://m.media-amazon.com/images/I/91bYsX41DVL._AC_SY400_.jpg' alt=''/>
-          <br/>
-          <br/>
-          <button>Add to Cart</button>
+      <div className='card-container'>
+        <div className='card'>
+          <h2>Fugiat sit voluptate elit excepteur dolor</h2>
+          <div className='product-card-nested'>
+            <div className='card-nested'>
+              <img src={image1} alt='Home storage' className='product-image' />
+              <p>Home storage</p>
+            </div>
+            <div className='card-nested'>
+              <img src={image2} alt='Lighting solutions' className='product-image' />
+              <p>Lighting solutions</p>
+            </div>
+          </div>
+          <div className='product-card-nested'>
+            <div className='card-nested'>
+              <img src={image3} alt='Vases, decor and more' className='product-image' />
+              <p>Vases, decor and more</p>
+            </div>
+            <div className='card-nested'>
+              <img src={image4} alt='Vases, decor and more' className='product-image' />
+              <p>Vases, decor and more</p>
+            </div>
+          </div>
+          <div className='textHolder'>
+            <button className='add-to-cart-btn'>Add to Cart</button>
+          </div>
+          <div className='link'>
+            <a href="#" className='explore-more-link'>Explore More</a>
+          </div>
         </div>
-      </div>
-      <div className='card'>
-        <p className='title'>Fugiat sit voluptate elit excepteur dolor eiusmod adipisicing ipsum labore qui sit pariatur laborum.</p>
-        <p className='price'>300.00</p>
-        <p className='starRating'>stars</p>
-        <div className='imageHolder'>
-          <img src='https://m.media-amazon.com/images/I/81bsw6fnUiL._AC_SY400_.jpg' alt=''/>
-          <br/>
-          <br/>
-          <button>Add to Cart</button>
+        {/* Repeat similar card structures as needed */}
+        <div className='card'>
+          {/* Content for the second card */}
         </div>
-      </div>
-      <div className='card'>
-        <p className='title'>Fugiat sit voluptate elit excepteur dolor eiusmod adipisicing ipsum labore qui sit pariatur laborum.</p>
-        <p className='price'>410.00</p>
-        <p className='starRating'>stars</p>
-        <div className='imageHolder'>
-          <img src='https://m.media-amazon.com/images/I/81l3rZK4lnL._AC_SY400_.jpg' alt=''/>
-          <br/>
-          <br/>
-          <button>Add to Cart</button>
-        </div>
-      </div>
-      <div className='card'>
-        <p className='title'>Fugiat sit voluptate elit excepteur dolor eiusmod adipisicing ipsum labore qui sit pariatur laborum.</p>
-        <p className='price'>610.00</p>
-        <p className='starRating'>stars</p>
-        <div className='imageHolder'>
-          <img src='https://m.media-amazon.com/images/I/81jv44QdNwL._AC_SY400_.jpg' alt=''/>
-          <br/>
-          <br/>
-          <button>Add to Cart</button>
-        </div>
-      </div>
-      <div className='card'>
-        <p className='title'>Fugiat sit voluptate elit excepteur dolor eiusmod adipisicing ipsum labore qui sit pariatur laborum.</p>
-        <p className='price'>110.00</p>
-        <p className='starRating'>stars</p>
-        <div className='imageHolder'>
-          <img src='https://m.media-amazon.com/images/I/818e+fq7+BL._AC_SY400_.jpg' alt=''/>
-          <br/>
-          <br/>
-          <button>Add to Cart</button>
-        </div>
-      </div>
-      <div className='card'>
-        <p className='title'>Fugiat sit voluptate elit excepteur dolor eiusmod adipisicing ipsum labore qui sit pariatur laborum.</p>
-        <p className='price'>910.00</p>
-        <p className='starRating'>stars</p>
-        <div className='imageHolder'>
-          <img src='https://m.media-amazon.com/images/I/71sBtM3Yi5L._AC_SY400_.jpg' alt=''/>
-          <br/>
-          <br/>
-          <button>Add to Cart</button>
-        </div>
-      </div>
-      <div className='card'>
-        <p className='title'>Fugiat sit voluptate elit excepteur dolor eiusmod adipisicing ipsum labore qui sit pariatur laborum.</p>
-        <p className='price'>410.00</p>
-        <p className='starRating'>stars</p>
-        <div className='imageHolder'>
-          <img src='https://m.media-amazon.com/images/I/710jnzKlDTL._AC_SY400_.jpg' alt=''/>
-          <br/>
-          <br/>
-          <button>Add to Cart</button>
-        </div>
-      </div>
-      <div className='card'>
-        <p className='title'>Fugiat sit voluptate elit excepteur dolor eiusmod adipisicing ipsum labore qui sit pariatur laborum.</p>
-        <p className='price'>10.00</p>
-        <p className='starRating'>stars</p>
-        <div className='imageHolder'>
-          <img src='https://m.media-amazon.com/images/I/81N7FmJhbhL._AC_SY400_.jpg' alt=''/>
-          <br/>
-          <br/>
-          <button>Add to Cart</button>
-        </div>
+        {/* Add more cards here */}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ProductList;
